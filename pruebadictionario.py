9aa734108415fcd21d6853b50056d1a764c90446
ErickLI1234 <163763478+ErickLI1234@users.noreply.github.com>
2024-03-17 11:14:42 -0700

    Create pruebadictionario.py

---
+++ pruebadictionario.py
@@ -0,0 +1,15 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "ROFL": "Una respuesta a una broma",
+            "SHEESH":"ligera desaprobacion",
+            "CREEPY": "aterrador,siniestro",
+            "AGGRO": "ponerse agresivo/enojado",
+            }
+for i in range(5):
+    word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ") 
+    if word in meme_dict.keys():
+       print(meme_dict[word])
+
+    else:
+       print("no esta la palabro lo siento")
